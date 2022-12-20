81058ef09bb10041671521dbb7e3a6d1e2194e9b
Athaliah <athaliahtauro@gmail.com>
2022-12-20 10:05:20 +0200

    FIX linter errors

--- src/index.js
+++ src/index.js
@@ -1,59 +1,44 @@
-/* eslint-disable no-unused-vars */
-/* eslint func-names: ["error", "never", { "generators": "always" }] */
-const myNodelist = document.getElementsByTagName('LI');
-let i;
-/* eslint-disable-next-line no-plusplus */
-for (i = 0; i < myNodelist.length; i++) {
-  const span = document.createElement('SPAN');
-  const txt = document.createTextNode('\u00D7');
-  span.className = 'close';
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+const ul = document.getElementById('list');
+let li;
+const addButton = document.getElementById('add');
+addButton.addEventListener('click', addItem);
 
-const close = document.getElementsByClassName('close');
-/* eslint-disable-next-line no-plusplus */
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
-    const div = this.parentElement;
-    div.style.display = 'none';
-  };
-}
-
-const list = document.querySelector('ul');
-list.addEventListener('click', (ev) => {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+function addItem() {
+  const input = document.getElementById('input');
+  const item = input.value;
+  const textNode = document.createTextNode(item);
+  if (item === '') {
+    msg = 'Enter your Task';
+    alert(msg);
+    return false;
   }
-}, false);
+  li = document.createElement('li');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.setAttribute('id', 'check');
 
-function newElement() {
-  const li = document.createElement('li');
-  const inputValue = document.getElementById('myInput').value;
-  const t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert('You must write something!');
-  } else {
-    document.getElementById('myUL').appendChild(li);
+  const label = document.createElement('label');
+
+  ul.appendChild(label);
+  li.appendChild(checkbox);
+  label.appendChild(textNode);
+  li.appendChild(label);
+  ul.insertBefore(li, ul.childNodes[0]);
+  setTimeout(() => {
+    li.className = 'visual';
+  }, 5);
+  input.value = ' ';
+
+  const removeButton = document.getElementById('remove');
+  removeButton.addEventListener('click', removeItem);
+
+  function removeItem() {
+    li = ul.children;
+    for (let index = 0; index < li.length; index++) {
+      const element = li[index];
+      while (li[index] && li[index].children[0].checked) {
+        ul.removeChild(li[index]);
+      }
+    }
   }
-  document.getElementById('myInput').value = '';
-
-  const span = document.createElement('SPAN');
-  const txt = document.createTextNode('\u00D7');
-  span.className = 'close';
-  span.appendChild(txt);
-  li.appendChild(span);
-  /* eslint-disable-next-line no-plusplus */
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      const div = this.parentElement;
-      div.style.display = 'none';
-    };
-  }
-}
-
-function removeAll() {
-  const lst = document.getElementsByTagName('ul');
-  lst[0].innerHTML = '';
 }
