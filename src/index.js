539eaa68943b74088c93088cb462f250c5a7f812
Athaliah <athaliahtauro@gmail.com>
2022-12-23 17:39:56 +0200

    Modify changes in list functionality and linter errors

--- src/index.js
+++ src/index.js
@@ -1,50 +1,19 @@
-/* eslint-disable no-use-before-define */
-/* eslint-disable max-classes-per-file */
+import './style.css';
+import List from './list.js';
 
-const ul = document.getElementById('list');
-let msg;
-let li;
-const addButton = document.getElementById('add');
-addButton.addEventListener('click', addItem);
+const todolist = new List();
+todolist.display();
 
-function addItem() {
-  const input = document.getElementById('input');
-  const item = input.value;
-  const textNode = document.createTextNode(item);
-  if (item === '') {
-    msg = 'Enter your Task';
-    alert(msg);
-    return true;
+document.querySelector('#form-area').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const activity = e.target.elements.activity.value;
+  todolist.addWork(activity);
+  e.target.reset();
+});
+document.querySelector('.complete-items').addEventListener('click', () => {
+  todolist.clearCompletedActivity();
+});
 
-    return false;
-  }
-  li = document.createElement('li');
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.setAttribute('id', 'check');
-
-  const label = document.createElement('label');
-
-  ul.appendChild(label);
-  li.appendChild(checkbox);
-  label.appendChild(textNode);
-  li.appendChild(label);
-  ul.insertBefore(li, ul.childNodes[0]);
-  setTimeout(() => {
-    li.className = 'visual';
-  }, 5);
-  input.value = ' ';
-
-  const removeButton = document.getElementById('remove');
-  removeButton.addEventListener('click', removeItem);
-
-  function removeItem() {
-    li = ul.children;
-    for (let index = 0; index < li.length; index++) {
-      const element = li[index];
-      while (li[index] && li[index].children[0].checked) {
-        ul.removeChild(li[index]);
-      }
-    }
-  }
-}
+document.querySelector('#delete-all').addEventListener('click', () => {
+  todolist.clearAll();
+});
